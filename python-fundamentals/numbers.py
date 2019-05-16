0c3dc1c0628bb0292468c77a8108f0d3b700cf9a
Christopher Martin <martincdev@gmail.com>
2019-05-15 22:11:30 -0500

    Wrote down notes for numbers

---
+++ python-fundamentals/numbers.py
@@ -0,0 +1,51 @@
+# Learning Python data types
+
+# Integers
+#
+# x = 4         Positive Int
+# y = -3938     Negative Int
+# z = 0         Zero
+#
+# Float - To use float, add decimal
+#
+# x = 5.0       Positive Float
+# y = -3484.5   Negative Float
+# z = 0.0       Zero (shows as 0.0)
+#
+# Also supports complex numbers ex:
+# x = 42j
+#
+# Integers/simple data types are objects under the hood
+# you can create new ones by calling methods associated with their types, ex:
+#
+# type(x)
+# >>> <class 'complex'>
+#
+# type(y)
+# >>> <class 'float'>
+#
+# a = 1
+# type(1)
+# >>> <class 'int'>
+#
+# int(5)
+# >>> 5
+#
+# float(3.0)
+# >>> 3.0
+#
+# x = 5
+# y = 3.0
+# x + y
+# >>> 8.0
+#
+# 6 / 2
+# >>> 3.0   Ints divided created a float
+#
+# 3 * 3
+# >>> 9
+#
+#
+#
+#
+
